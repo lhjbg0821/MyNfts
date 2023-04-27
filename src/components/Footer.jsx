ae3eb1131c832dbba81108fbe53df1e6084d0713
lhjbg0821 <82081355+lhjbg0821@users.noreply.github.com>
2023-04-27 21:04:33 +0900

    Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,14 +1,31 @@
-import { Link } from "react-router-dom";
+import { BsGithub, BsInstagram } from "react-icons/bs";
+import { SiVelog } from "react-icons/si";
+import { HiOutlineMail } from "react-icons/hi";
 
 const Footer = () => {
   return (
-    <div>
+    <div className="bg-gray-950 text-gray-50 px-4 py-6 font-bold text-xl">
       <div>OllOK</div>
-      <div>
-        Contact me.
-        <Link>https://github.com/lhjbg0821</Link>
+      <div>이현진</div>
+      <div className="mt-8">
+        <button className="bg-yellow-500 rounded-full mr-4">
+          <BsInstagram className="w-8 h-8" />
+          <a href="https://github.com/lhjbg0821"></a>
+        </button>
+        <button className="bg-yellow-500 rounded-full mr-4">
+          <BsGithub className="w-8 h-8" />
+          <a href="https://github.com/lhjbg0821"></a>
+        </button>
+        <button className="bg-yellow-500 rounded-full mr-4">
+          <SiVelog className="w-8 h-8" />
+          <a href="https://velog.io/@lhjbg0821"></a>
+        </button>
+        <button className="bg-yellow-500 rounded-full ">
+          <HiOutlineMail className="w-8 h-8" />
+          <a href="https://velog.io/@lhjbg0821"></a>
+        </button>
       </div>
-      <div>OllOK</div>
+      <div>Designed By. 현진</div>
     </div>
   );
 };
